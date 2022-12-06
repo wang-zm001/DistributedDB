8514b051a5f5a6a6607c8cc18a5d2749cea78cca
Wang Ziming <714003249@qq.com>
2022-12-06 20:45:57 +0800

    update shell

--- lunch.sh
+++ lunch.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-trap 'killall distribkv' SIGINT
+trap 'killall DistributedDB' SIGINT
 
 cd $(dirname $0)
 
-killall distribkv || true
+killall DistributedDB || true
 sleep 0.1
 
 go install -v
 
-distribkv -db-location=moscow.db -http-addr=127.0.0.1:8080 -config-file=sharding.toml -shard=Moscow &
-distribkv -db-location=minsk.db -http-addr=127.0.0.1:8081 -config-file=sharding.toml -shard=Minsk &
-distribkv -db-location=kiev.db -http-addr=127.0.0.1:8082 -config-file=sharding.toml -shard=Kiev &
-distribkv -db-location=tashkent.db -http-addr=127.0.0.1:8083 -config-file=sharding.toml -shard=Tashkent &
+DistributedDB -db-location=num0.db -http-addr=127.0.0.1:8080 -config-file=sharding.toml -shard=num0 &
+DistributedDB -db-location=num1.db -http-addr=127.0.0.1:8081 -config-file=sharding.toml -shard=num1 &
+DistributedDB -db-location=num2.db -http-addr=127.0.0.1:8082 -config-file=sharding.toml -shard=num2 &
+DistributedDB -db-location=num3.db -http-addr=127.0.0.1:8083 -config-file=sharding.toml -shard=num3 &
 
 wait
