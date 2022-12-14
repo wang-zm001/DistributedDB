9af8acdc778dc741a0de2a02d538019b457ef5b5
WangZiming <714003249@qq.com>
2022-12-14 16:33:24 +0800

    rm cache temporarily

--- db/db.go
+++ db/db.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 
 	bolt "github.com/boltdb/bolt"
-	cache "github.com/wang-zm001/DistributedDB/cache"
 )
 
 var (
@@ -16,7 +15,6 @@ var (
 
 type Database struct {
 	db       *bolt.DB
-	cache    *cache.Cache
 	readOnly bool
 }
 
